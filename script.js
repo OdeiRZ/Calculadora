32671f57cc6a5c73d78c7d9f1fc8597c6c4d1a79
Odei Riveiro Zafra <odei.riveiro@gmail.com>
2017-06-01 18:24:49 +0200

    Agregada función para mostrar lupa

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ var operPendiente="";
 function mostrar() {
 	numero.value=location.search.substring(1);
 }
+function lupa(pantalla,pagina) {
+	var ventana=window.open(pagina,"ventana","width=500,height=200");
+	ventana.moveTo((screen.width-500)/2,(screen.height-200)/2);
+	ventana.location.href=pagina+"?"+pantalla.value;
+	ventana.focus;
+}
